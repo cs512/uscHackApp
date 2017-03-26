88a91e0e6f1909aa81259dfbcd58d8402b505cb8
J. Austin Wang <wangjunzhuo200@gmail.com>
2017-03-26 11:41:12 -0700

    add network for checkin

--- src/pages/checkin/checkin.ts
+++ src/pages/checkin/checkin.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Http, RequestOptions, Headers } from '@angular/http';
 
 /*
   Generated class for the Checkin page.
@@ -15,7 +16,7 @@ import { NavController, NavParams } from 'ionic-angular';
 export class CheckinPage {
   
   public reservations: Reservation[] = [];//'{[{"building":"Leavey Library", "room":"A1", "startTime":"13:00 pm ~ 14:00 pm"}]}';
-  constructor(public navCtrl: NavController, public navParams: NavParams) { }
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) { }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CheckinPage');
@@ -23,12 +24,27 @@ export class CheckinPage {
 
   doRefresh(refresher) {
     console.log('Begin async operation', refresher);
-
-    setTimeout(() => {
-      console.log('Async operation has ended');
-      this.reservations.push(new Reservation("Leavey Library", "A1", "2017-03-31", "13:00 pm ~ 14:00 pm"));
-      refresher.complete();
-    }, 500);
+    // http://ec2-54-153-113-42.us-west-1.compute.amazonaws.com/bookroom/sql.php?email=trojan@usc.edu
+    this.http
+      .get('http://ec2-54-153-113-42.us-west-1.compute.amazonaws.com/bookroom/sql.php?email=trojan@usc.edu')
+      .subscribe(data => {
+        this.reservations = [];
+        let jsonData = data.json();
+        console.log(jsonData);
+        if (jsonData.status == 1) {
+          for(let i = 0; i < jsonData.num; ++i) {
+            let d = jsonData.info[i].date.substring(0, 4) + '-'
+              + jsonData.info[i].date.substring(4, 6) + '-'
+              + jsonData.info[i].date.substring(6, 8);
+            let t = eval(jsonData.info[i].time);
+            let ts = t + ':00 ~ ' + (t + 1) + ':00';
+            let r = new Reservation('Leavey Library', jsonData.info[i].room, d, ts);
+            this.reservations.push(r);
+            console.log(r);
+          }
+        }
+        refresher.complete();
+      })
   }
 
 }
